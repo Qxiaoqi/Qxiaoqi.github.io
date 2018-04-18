abb1afca72594d079cf109c7cb24d944ab339b4b
Qxiaoqi <308805548@qq.com>
2018-04-18 15:47:08 +0800

    Site updated: 2018-04-18 15:47:06

--- js/graffiti.js
+++ js/graffiti.js
@@ -32,6 +32,45 @@ var NavigationHover = function() {
 
 }
 
+/*var NavigationHover = function() {
+	var Home = document.getElementById('Home');
+	var Archives = document.getElementById('Archives');
+	var Tags = document.getElementById('Tags');
+	var Github = document.getElementById('Github');
+
+	var HomeHover = document.getElementsByClassName('Home-hover')[0];
+	var ArchivesHover = document.getElementsByClassName('Archives-hover')[0];
+	var TagsHover = document.getElementsByClassName('Tags-hover')[0];
+	var GithubHover = document.getElementsByClassName('Github-hover')[0];
+
+
+	Home.addEventListener('mouseover', function() {
+		console.log('test');
+		HomeHover.style.display = 'blcok';
+	});
+	Archives.addEventListener('mouseover', function() {
+		ArchivesHover.style.display = 'blcok';
+	});
+	Tags.addEventListener('mouseover', function() {
+		TagsHover.style.display = 'blcok';
+	});
+	Github.addEventListener('mouseover', function() {
+		GithubHover.style.display = 'blcok';
+	});
+}
+*/
+
+
+/*var mouseOver = function(event) {
+	var currentElement = event.target.parentNode.firstElementChild;
+	currentElement.style.display = "block";
+}
+
+var mouseOut = function(event) {
+	var currentElement = event.target.parentNode.firstElementChild;
+	currentElement.style.display = "none";
+}*/
+
 
 //================tag-icon颜色==========================
 
